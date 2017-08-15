b92b14c765e509d8ea1fa32bda13c2cde9e455a8
Peter Nociar <peter.nociar@digital.trade.gov.uk>
2017-08-15 17:05:17 +0100

    Django admin page

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn config.wsgi:application
-worker: celery worker --app=evemansys.taskapp --loglevel=info
+#worker: celery worker --app=evemansys.taskapp --loglevel=info
--- config/settings/staging.py
+++ config/settings/staging.py
@@ -165,7 +165,7 @@ SENTRY_CELERY_LOGLEVEL = env.int('DJANGO_SENTRY_LOG_LEVEL', logging.INFO)
 # }
 
 # Custom Admin URL, use {% url 'admin:index' %}
-ADMIN_URL = env('DJANGO_ADMIN_URL')
+ADMIN_URL = env('DJANGO_ADMIN_URL', '/admin')
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
