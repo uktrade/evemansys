93d146a81a41d3db15f228f9ffc0f4a959cde62f
Peter Nociar <peter.nociar@digital.trade.gov.uk>
2017-08-16 11:50:58 +0100

    cast to str

--- config/settings/base.py
+++ config/settings/base.py
@@ -189,7 +189,7 @@ STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
-    os.path.join(ROOT_DIR, 'static'),
+    str(os.path.join(ROOT_DIR, 'static')),
     str(APPS_DIR.path('static')),
 ]
 
