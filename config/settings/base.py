0d4992e249cf0721ffbb3b7af0ca9c6c3548f16d
Peter Nociar <peter.nociar@digital.trade.gov.uk>
2017-08-16 11:29:33 +0100

    Revert

--- config/settings/base.py
+++ config/settings/base.py
@@ -187,7 +187,6 @@ STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
-    STATIC_ROOT,
     str(APPS_DIR.path('static')),
 ]
 
